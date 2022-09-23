2c575966e06e12217671b78b4d1bc50ba2cfc50f
stella <sftamesis13@gmail.com>
2022-09-23 10:54:27 -0700

    Filter Deliverables all complete

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,25 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("") // Filter Deliverable #1, where to add state for this feature.
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
-  });
+  const itemsToDisplay = items
+  .filter(item => item.name.toLowerCase().includes(search.toLowerCase())) //Filter Deliverable #4
+  
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter 
+        onCategoryChange={handleCategoryChange} 
+        //Filter Deliverable #1: Passing state as props
+        search = {search}
+        onSearchChange={setSearch}
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
